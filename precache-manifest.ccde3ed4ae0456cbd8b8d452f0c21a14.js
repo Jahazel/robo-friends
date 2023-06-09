0b9f726a37bce35cebeeeb9e70278477746fa2d5
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2023-06-09 14:16:54 -0500

    Updates

---
+++ precache-manifest.ccde3ed4ae0456cbd8b8d452f0c21a14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc46ad76af0930b8f71b",
+    "url": "/robo-friends/static/css/main.0f4a442b.chunk.css"
+  },
+  {
+    "revision": "bc46ad76af0930b8f71b",
+    "url": "/robo-friends/static/js/main.f68bbddc.chunk.js"
+  },
+  {
+    "revision": "74ef7f23abc83f2f7b75",
+    "url": "/robo-friends/static/js/runtime~main.f49acb03.js"
+  },
+  {
+    "revision": "e037ae4a26125112047d",
+    "url": "/robo-friends/static/css/2.d1f4f839.chunk.css"
+  },
+  {
+    "revision": "e037ae4a26125112047d",
+    "url": "/robo-friends/static/js/2.b19faffd.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "/robo-friends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b31ecae23a3337621d914a8ba9046f66",
+    "url": "/robo-friends/index.html"
+  }
+];
